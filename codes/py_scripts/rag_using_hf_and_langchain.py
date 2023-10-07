ca7a8e1e5a0b9e30a4b2d4dba79b77c2f717c053
Karthik Soman <karthi.soman@gmail.com>
2023-10-06 17:48:58 -0700

    added CallbackManager

--- codes/py_scripts/rag_using_hf_and_langchain.py
+++ codes/py_scripts/rag_using_hf_and_langchain.py
@@ -1,5 +1,7 @@
 from langchain import HuggingFacePipeline
-from langchain import PromptTemplate,  LLMChain
+from langchain import PromptTemplate, LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from transformers import pipeline
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -45,6 +47,7 @@ pipe = pipeline("text-generation",
                 )
 
 llm = HuggingFacePipeline(pipeline = pipe, 
+                          callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])),
                           model_kwargs = {'temperature':0})
 
 
