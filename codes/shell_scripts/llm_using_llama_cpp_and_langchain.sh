1e87bb89f66e9cbf3d22d192a833dba926f0b78e
Karthik Soman <karthi.soman@gmail.com>
2023-10-09 17:04:32 -0700

    increased batch size for token processing

--- codes/shell_scripts/llm_using_llama_cpp_and_langchain.sh
+++ codes/shell_scripts/llm_using_llama_cpp_and_langchain.sh
@@ -1,6 +1,6 @@
 MODEL_PATH="/data1/somank/llm_data/llm_models/llama-2-13b-chat.Q5_K_M.gguf"
 N_GPU_LAYERS=40
-N_BATCH=512
+N_BATCH=1024
 
 
 python ../py_scripts/llm_using_llama_cpp_and_langchain.py $MODEL_PATH $N_GPU_LAYERS $N_BATCH
