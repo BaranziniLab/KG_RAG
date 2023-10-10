84012fd6658cb1396857a58044b48cead5fe8593
Karthik Soman <karthi.soman@gmail.com>
2023-10-09 19:00:45 -0700

    changed dataset name

--- codes/py_scripts/llm_using_hf_and_langchain.py
+++ codes/py_scripts/llm_using_hf_and_langchain.py
@@ -22,7 +22,7 @@ def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT):
 
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-gptq_config = GPTQConfig(bits=4, dataset="wikitext", tokenizer=tokenizer)
+gptq_config = GPTQConfig(bits=4, dataset="wikitext2", tokenizer=tokenizer)
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=gptq_config)
 streamer = TextStreamer(tokenizer)
 
