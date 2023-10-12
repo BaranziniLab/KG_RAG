357a9baafe4bd9e89443e8141df21912c3805052
Karthik Soman <karthi.soman@gmail.com>
2023-10-12 15:02:19 -0700

    adding 7B

--- codes/shell_scripts/true_false_generation_HuggingFace.sh
+++ codes/shell_scripts/true_false_generation_HuggingFace.sh
@@ -1,4 +1,4 @@
-MODEL_NAME="TheBloke/Llama-2-13b-Chat-GPTQ"
+MODEL_NAME="TheBloke/Llama-2-7b-Chat-GPTQ"
 BRANCH_NAME="gptq-4bit-64g-actorder_True"
 QUESTION_PATH="/data1/somank/llm_data/analysis/test_questions.csv"
 SAVE_PATH="/data1/somank/llm_data/analysis"
