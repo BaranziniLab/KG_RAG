72cf46452204e8ef83a9d4616f00dccd28a8a2fa
Karthik Soman <karthi.soman@gmail.com>
2023-10-13 23:17:13 -0700

    changed model to meta llama

--- codes/shell_scripts/true_false_generation_HuggingFace.sh
+++ codes/shell_scripts/true_false_generation_HuggingFace.sh
@@ -1,8 +1,8 @@
 MODEL_NAME="meta-llama/Llama-2-7b-chat-hf"
 BRANCH_NAME="main"
-QUESTION_PATH="/data1/somank/llm_data/analysis/test_questions.csv"
-SAVE_PATH="/data1/somank/llm_data/analysis"
+QUESTION_PATH="/data/somank/llm_data/analysis/test_questions.csv"
+SAVE_PATH="/data/somank/llm_data/analysis"
 STREAM="False"
-CACHE_DIR="/data1/somank/llm_data/llm_models/huggingface"
+CACHE_DIR="/data/somank/llm_data/llm_models/huggingface"
 
 python ../py_scripts/prompt_based_text_generation/true_false_generation_HuggingFace.py $MODEL_NAME $BRANCH_NAME $QUESTION_PATH $SAVE_PATH $STREAM $CACHE_DIR
