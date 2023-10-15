54229d751553c5a07deb849f308e2811d3b4e287
Karthik Soman <karthi.soman@gmail.com>
2023-10-15 02:16:30 -0700

    added langchain pipeline for HF

--- codes/py_scripts/prompt_based_text_generation/PubMedBERT/text_generation_PubMedBERT.py
+++ codes/py_scripts/prompt_based_text_generation/PubMedBERT/text_generation_PubMedBERT.py
@@ -17,9 +17,15 @@ pipe = pipeline("text-generation",
                 tokenizer = tokenizer,
                 torch_dtype = torch.bfloat16,
                 device_map = "auto",
-                streamer=streamer,
-                temperature=0.01
+                max_new_tokens = 30
+                streamer=streamer
                 )
 
 question = input("Enter you statement : ")
-pipe(question, max_length=30, num_return_sequences=1, do_sample=True)
+# pipe(question, max_length=30, num_return_sequences=1, do_sample=True)
+
+llm = HuggingFacePipeline(pipeline = pipe,
+                          model_kwargs = {"temperature":0, "top_p":1})
+
+llm(question)
+
