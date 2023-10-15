da772ab37a30ed0024deb71a4c5d4153a5799472
Karthik Soman <karthi.soman@gmail.com>
2023-10-15 10:37:06 -0700

    INCREASED length for pubmedbert

--- codes/py_scripts/prompt_based_text_generation/PubMedBERT/text_generation_PubMedBERT.py
+++ codes/py_scripts/prompt_based_text_generation/PubMedBERT/text_generation_PubMedBERT.py
@@ -17,7 +17,7 @@ pipe = pipeline("text-generation",
                 tokenizer = tokenizer,
                 torch_dtype = torch.bfloat16,
                 device_map = "auto",
-                max_length = 30,
+                max_length = 50,
                 streamer=streamer
                 )
 
