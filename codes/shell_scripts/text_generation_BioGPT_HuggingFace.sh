9cb8afbf65d8da86bfd936d1401e1cc90182a085
Karthik Soman <karthi.soman@gmail.com>
2023-10-15 00:24:58 -0700

    amended shell script for prompt based generation for BioGPT

--- codes/shell_scripts/text_generation_BioGPT_HuggingFace.sh
+++ codes/shell_scripts/text_generation_BioGPT_HuggingFace.sh
@@ -5,3 +5,5 @@ SAVE_PATH="/data/somank/llm_data/analysis"
 STREAM="False"
 CACHE_DIR="/data/somank/llm_data/llm_models/huggingface"
 
+python ../py_scripts/prompt_based_text_generation/BioGPT/true_false_generation_BioGPT_HuggingFace.py $MODEL_NAME $BRANCH_NAME $QUESTION_PATH $SAVE_PATH $STREAM $CACHE_DIR
+
