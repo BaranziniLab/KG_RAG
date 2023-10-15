55e9e3c93d06c3e1169df486aea4eff6ed7773de
Karthik Soman <karthi.soman@gmail.com>
2023-10-15 10:17:11 -0700

    adding galactica

--- codes/py_scripts/prompt_based_text_generation/Galactica/text_generation_Galactica.py
+++ codes/py_scripts/prompt_based_text_generation/Galactica/text_generation_Galactica.py
@@ -1,5 +1,5 @@
 # pip install accelerate
-from transformers import AutoTokenizer, OPTForCausalLM
+from transformers import AutoTokenizer, OPTForCausalLM, pipeline, TextStreamer
 
 MODEL_NAME = "facebook/galactica-1.3b"
 CACHE_DIR = "/data/somank/llm_data/llm_models/huggingface"
@@ -7,9 +7,23 @@ CACHE_DIR = "/data/somank/llm_data/llm_models/huggingface"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, cache_dir=CACHE_DIR)
 model = OPTForCausalLM.from_pretrained(MODEL_NAME, device_map="auto", cache_dir=CACHE_DIR)
 
-# input_text = "The Transformer architecture [START_REF]"
-input_text = input("Enter your text : ")
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+streamer = TextStreamer(tokenizer)
 
-outputs = model.generate(input_ids)
-print(tokenizer.decode(outputs[0]))
+pipe = pipeline("text-generation",
+                model = model,
+                tokenizer = tokenizer,
+                torch_dtype = torch.bfloat16,
+                device_map = "auto",
+                max_length = 30,
+                streamer=streamer
+                )
+
+input_text = input("Enter your text : ")
+pipe(input_text, max_length=30, num_return_sequences=1, do_sample=True)
+
+# input_text = "The Transformer architecture [START_REF]"
+
+# input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+# outputs = model.generate(input_ids)
+# print(tokenizer.decode(outputs[0]))
