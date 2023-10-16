381306fe0e4ef4b75b2fba3ec1f074454380d787
Karthik Soman <karthi.soman@gmail.com>
2023-10-16 01:11:33 -0700

    reduced return max_tokens

--- codes/py_scripts/prompt_based_text_generation/Galactica/true_false_generation_Galactica_HuggingFace.py
+++ codes/py_scripts/prompt_based_text_generation/Galactica/true_false_generation_Galactica_HuggingFace.py
@@ -32,7 +32,7 @@ def model(MODEL_NAME, BRANCH_NAME):
                 tokenizer = tokenizer,
                 torch_dtype = torch.bfloat16,
                 device_map = "auto",
-                max_new_tokens = 150,
+                max_new_tokens = 100,
                 do_sample = False
                 )
 
