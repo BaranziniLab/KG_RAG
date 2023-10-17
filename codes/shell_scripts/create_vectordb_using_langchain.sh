7b4f8086117a2af4fab9a5dd12298e37b7ef1b1f
Karthik Soman <karthi.soman@gmail.com>
2023-10-17 03:16:26 -0700

    adding script for new vectorDB

--- codes/shell_scripts/create_vectordb_using_langchain.sh
+++ codes/shell_scripts/create_vectordb_using_langchain.sh
@@ -1,9 +1,9 @@
-DATA_PATH="/data1/somank/llm_data/spoke_data/disease_context_from_dev.pickle"
+DATA_PATH="/data1/somank/llm_data/spoke_data/disease_with_relation_to_genes.pickle"
 CHUNK_SIZE=650
 CHUNK_OVERLAP=200
 BATCH_SIZE=200
-SENTENCE_EMBEDDING_MODEL="multi-qa-mpnet-base-dot-v1"
-VECTOR_DB_NAME="/data1/somank/llm_data/vectorDB/disease_context_chromaDB_using_multi_qa_mpnet_base_dot_v1_sentence_transformer_model_with_chunk_size_650"
+SENTENCE_EMBEDDING_MODEL="pritamdeka/S-PubMedBert-MS-MARCO"
+VECTOR_DB_NAME="/data1/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_pubmed_bert_sentence_transformer_model_with_chunk_size_650"
 
 start_time=$(date +%s)
 python ../py_scripts/create_vectordb_using_langchain.py $DATA_PATH $CHUNK_SIZE $CHUNK_OVERLAP $BATCH_SIZE $SENTENCE_EMBEDDING_MODEL $VECTOR_DB_NAME
