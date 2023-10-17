7bb04dd0cbdd12172aaa7977cacf68882eebf239
Karthik Soman <karthi.soman@gmail.com>
2023-10-17 03:44:20 -0700

    creating another vectorDB

--- codes/shell_scripts/create_vectordb_using_langchain.sh
+++ codes/shell_scripts/create_vectordb_using_langchain.sh
@@ -2,8 +2,8 @@ DATA_PATH="/data1/somank/llm_data/spoke_data/disease_with_relation_to_genes.pick
 CHUNK_SIZE=650
 CHUNK_OVERLAP=200
 BATCH_SIZE=200
-SENTENCE_EMBEDDING_MODEL="pritamdeka/S-PubMedBert-MS-MARCO"
-VECTOR_DB_NAME="/data1/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_pubmed_bert_sentence_transformer_model_with_chunk_size_650"
+SENTENCE_EMBEDDING_MODEL="sentence-transformers/all-MiniLM-L6-v2"
+VECTOR_DB_NAME="/data1/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_all_MiniLM_L6_v2_sentence_transformer_model_with_chunk_size_650"
 
 start_time=$(date +%s)
 python ../py_scripts/vectorDB/create_vectordb_using_langchain.py $DATA_PATH $CHUNK_SIZE $CHUNK_OVERLAP $BATCH_SIZE $SENTENCE_EMBEDDING_MODEL $VECTOR_DB_NAME
