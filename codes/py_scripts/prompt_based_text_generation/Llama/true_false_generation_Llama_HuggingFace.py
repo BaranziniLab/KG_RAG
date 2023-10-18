b8e7831dd0e51cb529a28166343de9d93f354416
Karthik Soman <karthi.soman@gmail.com>
2023-10-18 12:18:22 -0700

    set do_sample=False

--- codes/py_scripts/prompt_based_text_generation/Llama/true_false_generation_Llama_HuggingFace.py
+++ codes/py_scripts/prompt_based_text_generation/Llama/true_false_generation_Llama_HuggingFace.py
@@ -12,18 +12,13 @@ MODEL_NAME = sys.argv[1]
 BRANCH_NAME = sys.argv[2]
 QUESTION_PATH = sys.argv[3]
 SAVE_PATH = sys.argv[4]
-STREAM = sys.argv[5]
-CACHE_DIR = sys.argv[6]
+CACHE_DIR = sys.argv[5]
 
 
 
 # MODEL_NAME = "TheBloke/Llama-2-13B-chat-GPTQ"
 # BRANCH_NAME = "gptq-4bit-64g-actorder_True"
 
-stream_dict = {
-    "True" : True,
-    "False" : False
-}
 
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
@@ -54,13 +49,31 @@ OR
 INSTRUCTION = "Question: {question}"
 
 
+def main():    
+    llm = model(MODEL_NAME, BRANCH_NAME, stream=stream_dict[STREAM])               
+    template = get_prompt(INSTRUCTION, SYSTEM_PROMPT)
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    start_time = time.time()
+    SAVE_NAME = "_".join(MODEL_NAME.split("/")[-1].split("-"))+"_prompt_based_response.csv"
+    question_df = pd.read_csv(QUESTION_PATH)
+    answer_list = []
+    for index, row in question_df.iterrows():
+        question = row["text"]
+        output = llm_chain.run(question)
+        answer_list.append((row["text"], row["label"], output))
+    answer_df = pd.DataFrame(answer_list, columns=["question", "label", "llm_answer"])
+    answer_df.to_csv(os.path.join(SAVE_PATH, SAVE_NAME), index=False, header=True)    
+    print("Completed in {} min".format((time.time()-start_time)/60))
+
+
 def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT):
     SYSTEM_PROMPT = B_SYS + new_system_prompt + E_SYS
     prompt_template =  B_INST + SYSTEM_PROMPT + instruction + E_INST
     return prompt_template
 
 
-def model(MODEL_NAME, BRANCH_NAME, stream=False):
+def model(MODEL_NAME, BRANCH_NAME):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME,
                                              use_auth_token=True,
                                              cache_dir=CACHE_DIR)
@@ -72,58 +85,19 @@ def model(MODEL_NAME, BRANCH_NAME, stream=False):
                                         cache_dir=CACHE_DIR
                                         )
     # gptq_config = GPTQConfig(bits=4, group_size=64, desc_act=True)
-    if stream:
-        streamer = TextStreamer(tokenizer)
-        pipe = pipeline("text-generation",
-                    model = model,
-                    tokenizer = tokenizer,
-                    torch_dtype = torch.bfloat16,
-                    device_map = "auto",
-                    max_new_tokens = 512,
-                    do_sample = True,
-                    top_k = 30,
-                    num_return_sequences = 1,
-                    streamer=streamer
-                    )
-
-    else:
-        pipe = pipeline("text-generation",
-                    model = model,
-                    tokenizer = tokenizer,
-                    torch_dtype = torch.bfloat16,
-                    device_map = "auto",
-                    max_new_tokens = 512,
-                    do_sample = True,
-                    top_k = 30,
-                    num_return_sequences = 1
-                    )
-
-    
+    pipe = pipeline("text-generation",
+                model = model,
+                tokenizer = tokenizer,
+                torch_dtype = torch.bfloat16,
+                device_map = "auto",
+                max_new_tokens = 512,
+                do_sample = False
+                )    
     llm = HuggingFacePipeline(pipeline = pipe,
                               model_kwargs = {"temperature":0, "top_p":1})
     return llm
 
-def main():    
-    llm = model(MODEL_NAME, BRANCH_NAME, stream=stream_dict[STREAM])               
-    template = get_prompt(INSTRUCTION, SYSTEM_PROMPT)
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
-    if QUESTION_PATH:
-        start_time = time.time()
-        SAVE_NAME = "_".join(MODEL_NAME.split("/")[-1].split("-"))+"_prompt_based_response.csv"
-        question_df = pd.read_csv(QUESTION_PATH)
-        answer_list = []
-        for index, row in question_df.iterrows():
-            question = row["text"]
-            output = llm_chain.run(question)
-            answer_list.append((row["text"], row["label"], output))
-        answer_df = pd.DataFrame(answer_list, columns=["question", "label", "llm_answer"])
-        answer_df.to_csv(os.path.join(SAVE_PATH, SAVE_NAME), index=False, header=True)    
-        print("Completed in {} min".format((time.time()-start_time)/60))
-    else:
-        question = input("Enter your question : ")
-        output = llm_chain.run(question)
-        print(output)
+
 
 if __name__ == "__main__":
     main()
--- codes/shell_scripts/true_false_generation_Llama_HuggingFace.sh
+++ codes/shell_scripts/true_false_generation_Llama_HuggingFace.sh
@@ -2,7 +2,6 @@ MODEL_NAME="meta-llama/Llama-2-13b-chat-hf"
 BRANCH_NAME="main"
 QUESTION_PATH="/data/somank/llm_data/analysis/test_questions.csv"
 SAVE_PATH="/data/somank/llm_data/analysis"
-STREAM="False"
 CACHE_DIR="/data/somank/llm_data/llm_models/huggingface"
 
-python ../py_scripts/prompt_based_text_generation/Llama/true_false_generation_Llama_HuggingFace.py $MODEL_NAME $BRANCH_NAME $QUESTION_PATH $SAVE_PATH $STREAM $CACHE_DIR
+python ../py_scripts/prompt_based_text_generation/Llama/true_false_generation_Llama_HuggingFace.py $MODEL_NAME $BRANCH_NAME $QUESTION_PATH $SAVE_PATH $CACHE_DIR
