8d5b99547eeced1bbcd73d7e5442436947422b02
Karthik Soman <karthi.soman@gmail.com>
2023-10-18 19:59:25 -0700

    rag retrieval threshold changed to 13b

--- codes/shell_scripts/rag_with_retrieval_threshold_for_true_false_generation_Llama_HuggingFace.sh
+++ codes/shell_scripts/rag_with_retrieval_threshold_for_true_false_generation_Llama_HuggingFace.sh
@@ -1,6 +1,6 @@
 VECTOR_DB_PATH="/data1/somank/llm_data/vectorDB/disease_context_chromaDB_using_pubmed_bert_sentence_transformer_model_with_chunk_size_650"
 SENTENCE_EMBEDDING_MODEL="pritamdeka/S-PubMedBert-MS-MARCO"
-MODEL_NAME="meta-llama/Llama-2-7b-chat-hf"
+MODEL_NAME="meta-llama/Llama-2-13b-chat-hf"
 BRANCH_NAME="main"
 QUESTION_PATH="/data1/somank/llm_data/analysis/test_questions.csv"
 SAVE_PATH="/data1/somank/llm_data/analysis"
