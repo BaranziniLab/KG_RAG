a8ea2bd9a2516d98f9a31f552aa9ad5f018f6100
Karthik Soman <karthi.soman@gmail.com>
2023-10-18 17:39:23 -0700

    changed file path

--- codes/shell_scripts/true_false_generation_Llama_HuggingFace.sh
+++ codes/shell_scripts/true_false_generation_Llama_HuggingFace.sh
@@ -1,7 +1,7 @@
 MODEL_NAME="meta-llama/Llama-2-13b-chat-hf"
 BRANCH_NAME="main"
-QUESTION_PATH="/data/somank/llm_data/analysis/test_questions.csv"
-SAVE_PATH="/data/somank/llm_data/analysis"
-CACHE_DIR="/data/somank/llm_data/llm_models/huggingface"
+QUESTION_PATH="/data1/somank/llm_data/analysis/test_questions.csv"
+SAVE_PATH="/data1/somank/llm_data/analysis"
+CACHE_DIR="/data1/somank/llm_data/llm_models/huggingface"
 
 python ../py_scripts/prompt_based_text_generation/Llama/true_false_generation_Llama_HuggingFace.py $MODEL_NAME $BRANCH_NAME $QUESTION_PATH $SAVE_PATH $CACHE_DIR
