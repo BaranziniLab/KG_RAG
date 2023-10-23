fd6d5589a8f2e66ee78decdc0c220643efa098e9
Karthik Soman <karthi.soman@gmail.com>
2023-10-23 02:04:59 -0700

    graph traversal script

--- codes/py_scripts/rag_based_text_generation/GPT/graph_traversal.py
+++ codes/py_scripts/rag_based_text_generation/GPT/graph_traversal.py
@@ -22,6 +22,29 @@ sys.path.insert(0, "../../")
 from utility import *
 
 
+def retrieve_context(question):
+    node_hits = vectorstore.similarity_search_with_score(question, k=MAX_NODE_HITS)
+    print("Node hits:\n")    
+    question_embedding = embedding_function.embed_query(question)
+    node_context_extracted = ""
+    for node in node_hits:
+        node_name = node[0].page_content
+        print(node_name)
+        node_context = node_context_df[node_context_df.node_name == node_name].node_context.values[0]
+        node_context_list = node_context.split(". ")        
+        node_context_embeddings = embedding_function.embed_documents(node_context_list)
+        similarities = [cosine_similarity(np.array(question_embedding).reshape(1, -1), np.array(node_context_embedding).reshape(1, -1)) for node_context_embedding in node_context_embeddings]
+        similarities = sorted([(e, i) for i, e in enumerate(similarities)], reverse=True)
+        percentile_threshold = np.percentile([s[0] for s in similarities], QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD)
+        high_similarity_indices = [s[1] for s in similarities if s[0] > percentile_threshold and s[0] > QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY]
+        if len(high_similarity_indices) > max_number_of_high_similarity_context_per_node:
+            high_similarity_indices = high_similarity_indices[:max_number_of_high_similarity_context_per_node]
+        high_similarity_context = [node_context_list[index] for index in high_similarity_indices]
+        node_context_extracted += ". ".join(high_similarity_context)
+        node_context_extracted += ". "
+    return node_context_extracted
+
+
 CHAT_MODEL_ID = "gpt-35-turbo"
 CHAT_DEPLOYMENT_ID = None
 VECTOR_DB_PATH = "/data/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_all_MiniLM_L6_v2_sentence_transformer_model_with_chunk_size_650"
@@ -67,7 +90,12 @@ MAX_NUMBER_OF_CONTEXT_FOR_A_QUESTION = 150
 max_number_of_high_similarity_context_per_node = int(MAX_NUMBER_OF_CONTEXT_FOR_A_QUESTION/MAX_NODE_HITS)
 node_context_df = pd.read_csv(NODE_CONTEXT_PATH)
 
-system_prompt = "You are an expert biomedical researcher"
+
+
+
+system_prompt = """
+You are an expert biomedical researcher. For answering the Question at the end, you need to first read the Context provided. Then give your final answer by considering the context and your inherent knowledge on the topic. If you don't know the answer, report as "I don't know", don't try to make up an answer.
+"""
 
 # Step 1
 instruction_1 = """
@@ -77,19 +105,10 @@ Give the answer in the following format:
 answer : <list of diseases>
 }}
 """
-
-completion = openai.ChatCompletion.create(
-    temperature=temperature, 
-    top_p=1,
-    deployment_id=CHAT_DEPLOYMENT_ID,
-    model=CHAT_MODEL_ID,
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": instruction_1}
-    ]
-)
-
-step1_answer = completion.choices[0].message["content"]
+step1_context = "Context: "+ retrieve_context(instruction_1)
+step1_enriched_prompt = step1_context + "\n" + "Question: " + instruction_1
+step1_output = get_GPT_response(step1_enriched_prompt, system_prompt, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=temperature)
+step1_answer = step1_output.choices[0].message["content"]
 extracted_diseases = step1_answer.split("answer : [")[-1].split("]")[0]
 
 # Step 2
@@ -97,19 +116,10 @@ instruction_2 = """
 What Compounds are used to treat each of the following diseases?
 {}
 """.format(extracted_diseases)
-
-completion = openai.ChatCompletion.create(
-    temperature=temperature, 
-    top_p=1,
-    deployment_id=CHAT_DEPLOYMENT_ID,
-    model=CHAT_MODEL_ID,
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": instruction_2}
-    ]
-)
-
-step2_answer = completion.choices[0].message["content"]
+step2_context = "Context: "+ retrieve_context(instruction_2)
+step2_enriched_prompt = step2_context + "\n" + "Question: " + instruction_2
+step2_output = get_GPT_response(step2_enriched_prompt, system_prompt, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=temperature)
+step2_answer = step2_output.choices[0].message["content"]
 print(step2_answer)
 
 
