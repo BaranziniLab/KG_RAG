e513c8af8adb81f998489471cc46f21bba327965
Karthik Soman <karthi.soman@gmail.com>
2023-10-24 14:33:27 -0700

    drug repor on 60 threshold

--- codes/py_scripts/rag_based_text_generation/GPT/drug_repurposing.py
+++ codes/py_scripts/rag_based_text_generation/GPT/drug_repurposing.py
@@ -22,9 +22,9 @@ SAVE_PATH = "/data/somank/llm_data/analysis"
 MAX_NODE_HITS = 1
 MAX_NUMBER_OF_CONTEXT_FOR_A_QUESTION = 150
 QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY = 0.5
-QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD = 70
+QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD = 60
 
-save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_node_retrieval_rag_based_drug_reporposing_questions_70_threshold.csv"
+save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_node_retrieval_rag_based_drug_reporposing_questions_60_threshold.csv"
 
 # GPT config params
 temperature = 0
