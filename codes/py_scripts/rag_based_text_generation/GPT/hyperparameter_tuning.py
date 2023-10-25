829c297803266acba79ea91d4e7ace17644ea3b4
Karthik Soman <karthi.soman@gmail.com>
2023-10-24 21:53:30 -0700

    adding hyper parameter tuning

--- codes/py_scripts/rag_based_text_generation/GPT/hyperparameter_tuning.py
+++ codes/py_scripts/rag_based_text_generation/GPT/hyperparameter_tuning.py
@@ -56,11 +56,11 @@ def main():
                 context = "Context: "+ retrieve_context(question, MAX_NODE_HITS, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD, max_number_of_high_similarity_context_per_node)
                 enriched_prompt = context + "\n" + "Question: " + question
                 output = get_GPT_response(enriched_prompt, system_prompt, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=temperature)
-                answer_list.append((row["disease_1"], row["Compounds"], row["Diseases"], row["text"], output, MAX_NODE_HITS, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD))
-                time.sleep(90)
+                answer_list.append((row["disease_1"], row["Compounds"], row["Diseases"], row["text"], output, MAX_NODE_HITS, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD))                
         answer_df = pd.DataFrame(answer_list, columns=["disease", "compound_groundTruth", "disease_groundTruth", "text", "llm_answer", "max_node_hits", "context_similarity_threshold"])
         save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_node_retrieval_rag_based_drug_reporposing_questions_parameter_tuning_round_{}.csv".format(node_hit_index+1)
         answer_df.to_csv(os.path.join(SAVE_PATH, save_name), index=False, header=True)
+        time.sleep(90)
     print("Completed in {} min".format((time.time()-start_time)/60))
     
 
