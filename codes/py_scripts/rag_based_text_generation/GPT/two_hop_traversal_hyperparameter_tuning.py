b927b8e824ec91f88ee46a05595d0c259f93e2a5
Karthik Soman <karthi.soman@gmail.com>
2023-10-27 20:01:00 -0700

    adding pubmed bert for sentence transformer

--- codes/py_scripts/rag_based_text_generation/GPT/two_hop_traversal_hyperparameter_tuning.py
+++ codes/py_scripts/rag_based_text_generation/GPT/two_hop_traversal_hyperparameter_tuning.py
@@ -13,7 +13,8 @@ CHAT_MODEL_ID = "gpt-4"
 CHAT_DEPLOYMENT_ID = None
 VECTOR_DB_PATH = "/data/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_all_MiniLM_L6_v2_sentence_transformer_model_with_chunk_size_650"
 NODE_CONTEXT_PATH = "/data/somank/llm_data/spoke_data/context_of_disease_which_has_relation_to_genes.csv"
-SENTENCE_EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL = "sentence-transformers/all-MiniLM-L6-v2"
+SENTENCE_EMBEDDING_MODEL_FOR_CONTEXT_RETRIEVAL = "pritamdeka/S-PubMedBert-MS-MARCO"
 QUESTION_PATH = "/data/somank/llm_data/analysis/disease_two_hop_validation_data.csv"
 SAVE_PATH = "/data/somank/llm_data/analysis"
 
@@ -36,8 +37,9 @@ system_prompt = """
     {{Nodes:<list of nodes>}}
 """
 
-embedding_function = SentenceTransformerEmbeddings(model_name=SENTENCE_EMBEDDING_MODEL)
-vectorstore = Chroma(persist_directory=VECTOR_DB_PATH, embedding_function=embedding_function)
+embedding_function_for_node_retrieval = SentenceTransformerEmbeddings(model_name=SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL)
+embedding_function_for_context_retrieval = SentenceTransformerEmbeddings(model_name=SENTENCE_EMBEDDING_MODEL_FOR_CONTEXT_RETRIEVAL)
+vectorstore = Chroma(persist_directory=VECTOR_DB_PATH, embedding_function=embedding_function_for_node_retrieval)
 
 def main():
     start_time = time.time()
@@ -46,14 +48,14 @@ def main():
         answer_list = []
         for index, row in question_df.iterrows():
             question = row["text"]
-            context = retrieve_context(question, vectorstore, embedding_function, node_context_df, context_volume, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD, QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY)
+            context = retrieve_context(question, vectorstore, embedding_function_for_context_retrieval, node_context_df, context_volume, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD, QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY)
             enriched_prompt = "Context: "+ context + "\n" + "Question: " + question
             output = get_GPT_response(enriched_prompt, system_prompt, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=temperature)
             if not output:
                 time.sleep(5)
             answer_list.append((row["disease_1"], row["disease_2"], row["central_nodes"], row["text"], output, context_volume))
         answer_df = pd.DataFrame(answer_list, columns=["disease_1", "disease_2", "central_nodes_groundTruth", "text", "llm_answer", "context_volume"])
-        save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_node_retrieval_rag_based_two_hop_questions_parameter_tuning_round_{}.csv".format(context_index+1)
+        save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_PubMedBert_node_retrieval_rag_based_two_hop_questions_parameter_tuning_round_{}.csv".format(context_index+1)
         answer_df.to_csv(os.path.join(SAVE_PATH, save_name), index=False, header=True)
     print("Completed in {} min".format((time.time()-start_time)/60))
     
