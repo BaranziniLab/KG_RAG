f60402075b96587d94d3902190574eafa9c3aae6
Karthik Soman <karthi.soman@gmail.com>
2023-11-02 21:14:09 -0700

    updated drug repurposing questions

--- codes/py_scripts/rag_based_text_generation/GPT/drug_repurposing.py
+++ codes/py_scripts/rag_based_text_generation/GPT/drug_repurposing.py
@@ -10,7 +10,7 @@ VECTOR_DB_PATH = "/data/somank/llm_data/vectorDB/disease_nodes_chromaDB_using_al
 NODE_CONTEXT_PATH = "/data/somank/llm_data/spoke_data/context_of_disease_which_has_relation_to_genes.csv"
 SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL = "sentence-transformers/all-MiniLM-L6-v2"
 SENTENCE_EMBEDDING_MODEL_FOR_CONTEXT_RETRIEVAL = "pritamdeka/S-PubMedBert-MS-MARCO"
-QUESTION_PATH = "/data/somank/llm_data/analysis/drug_repurposing_questions.csv"
+QUESTION_PATH = "/data/somank/llm_data/analysis/drug_repurposing_questions_v2.csv"
 SAVE_PATH = "/data/somank/llm_data/analysis"
 
 
@@ -19,7 +19,7 @@ QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD = 75
 QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY = 0.5
 
 
-save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_entity_recognition_based_node_retrieval_rag_based_drug_repurposing_questions_response.csv"
+save_name = "_".join(CHAT_MODEL_ID.split("-"))+"_entity_recognition_based_node_retrieval_rag_based_drug_repurposing_questions_response_v2.csv"
 
 # GPT config params
 temperature = 0
@@ -47,8 +47,8 @@ def main():
         context = retrieve_context(question, vectorstore, embedding_function_for_context_retrieval, node_context_df, CONTEXT_VOLUME, QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD, QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY)
         enriched_prompt = "Context: " + context + "\n" + "Question: " + question
         output = get_GPT_response(enriched_prompt, system_prompt, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=temperature)
-        answer_list.append((row["disease_1"], row["Compounds"], row["Diseases"], row["text"], output))
-    answer_df = pd.DataFrame(answer_list, columns=["disease", "compound_groundTruth", "disease_groundTruth", "text", "llm_answer"])
+        answer_list.append((row["disease_in_question"], row["refDisease"], row["compoundGroundTruth"], row["text"], output))
+    answer_df = pd.DataFrame(answer_list, columns=["disease_in_question", "refDisease", "compoundGroundTruth", "text", "llm_answer"])
     answer_df.to_csv(os.path.join(SAVE_PATH, save_name), index=False, header=True)
     print("Completed in {} min".format((time.time()-start_time)/60))
 
