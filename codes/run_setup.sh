81405f83ae7cb9989bc9d9ee529e7ddec96a98fd
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 15:49:48 -0800

    amended shell script if condition

--- codes/run_setup.sh
+++ codes/run_setup.sh
@@ -2,7 +2,7 @@ echo ""
 echo "Starting to set up KG-RAG ..."
 echo ""
 
-echo "Checking for disease concepts vectorDB, if it does not exist, it will be created ..."
+echo "Checking disease concepts vectorDB ..."
 CONFIG_FILE="../config.yaml"
 VECTOR_DB_PATH=$(awk -F: '/VECTOR_DB_PATH/ {gsub(/ /, "", $2); print $2}' "$CONFIG_FILE")
 VECTOR_DB_PATH=$(echo "$VECTOR_DB_PATH" | tr -d \')
