2a06c36813f9d17fc5aaee5ab2f6a0e54ae1ce66
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 12:44:37 -0800

    adding run_setup.sh

---
+++ codes/run_setup.sh
@@ -0,0 +1,10 @@
+echo "Starting setting up KG-RAG ..."
+
+echo "Checking vectorDB for disease concepts ..."
+VECTOR_DB_PATH=$(cat "$CONFIG_FILE" | grep "VECTOR_DB_PATH" | cut -d ':' -f2 | tr -d '[:space:]')
+if [ -d "$VECTOR_DB_PATH" ]; then
+    echo "vectorDB already exists!"
+else
+    echo "Creating vectorDB ..."
+    python ../py_scripts/vectorDB/create_vectordb.py
+fi
