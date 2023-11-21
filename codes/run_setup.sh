a7ba01d486780904d8bdee85745c71d858e4dc01
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 15:29:57 -0800

    changed import structure in utility

--- codes/run_setup.sh
+++ codes/run_setup.sh
@@ -2,6 +2,7 @@ echo "Starting to set up KG-RAG ..."
 echo ""
 
 echo "Checking vectorDB for disease concepts ..."
+CONFIG_FILE="../config.yaml"
 VECTOR_DB_PATH=$(cat "$CONFIG_FILE" | grep "VECTOR_DB_PATH" | cut -d ':' -f2 | tr -d '[:space:]')
 if [ -d "$VECTOR_DB_PATH" ]; then
     echo "vectorDB already exists!"
