8e0166a7161a12409650747050085504ed269882
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 15:49:05 -0800

    amended shell script if condition

--- codes/run_setup.sh
+++ codes/run_setup.sh
@@ -1,9 +1,11 @@
+echo ""
 echo "Starting to set up KG-RAG ..."
 echo ""
 
-echo "Checking vectorDB for disease concepts ..."
+echo "Checking for disease concepts vectorDB, if it does not exist, it will be created ..."
 CONFIG_FILE="../config.yaml"
-VECTOR_DB_PATH=$(cat "$CONFIG_FILE" | grep "VECTOR_DB_PATH" | cut -d ':' -f2 | tr -d '[:space:]')
+VECTOR_DB_PATH=$(awk -F: '/VECTOR_DB_PATH/ {gsub(/ /, "", $2); print $2}' "$CONFIG_FILE")
+VECTOR_DB_PATH=$(echo "$VECTOR_DB_PATH" | tr -d \')
 if [ -d "$VECTOR_DB_PATH" ]; then
     echo "vectorDB already exists!"
 else
