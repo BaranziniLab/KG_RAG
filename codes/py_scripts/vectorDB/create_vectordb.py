99f7a90faf6bada164825f78ff8cc676448b1b9e
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 01:10:00 -0800

    re-arranged vectorDB folder

--- codes/py_scripts/vectorDB/create_vectordb.py
+++ codes/py_scripts/vectorDB/create_vectordb.py
@@ -1,30 +1,40 @@
-import chromadb
 import pickle
 import time
+import sys
+sys.path.insert(0,"..")
+from utility import RecursiveCharacterTextSplitter, Chroma, SentenceTransformerEmbeddings
+from config_loaded import *
 
-start_time = time.time()
 
-TEXTUAL_DATA_PATH_LIST = ["../data/disease_gene_textual_knowledge.pickle", "../data/disease_resembles_disease_textual_knowledge.pickle"]
-db_name = "spoke_subset_knowledge"
-metadata_name = "spoke_knowledge"
 
-def get_data():
-    textual_knowledge = []
-    for TEXTUAL_DATA_PATH in TEXTUAL_DATA_PATH_LIST:
-        with open(TEXTUAL_DATA_PATH, "rb") as f:
-            textual_knowledge.extend(pickle.load(f))
-    return textual_knowledge
-        
+DATA_PATH = file_path_data["DISEASE_ENTITY_PATH"]
+VECTOR_DB_NAME = file_path_data["VECTOR_DB_PATH"]
+CHUNK_SIZE = int(config_data["VECTOR_DB_CHUNK_SIZE"])
+CHUNK_OVERLAP = int(config_data["VECTOR_DB_CHUNK_OVERLAP"])
+BATCH_SIZE = int(config_data["VECTOR_DB_BATCH_SIZE"])
+SENTENCE_EMBEDDING_MODEL = config_data["VECTOR_DB_SENTENCE_EMBEDDING_MODEL"]
 
-# Persistent client
-textual_knowledge = get_data()
-client = chromadb.PersistentClient(path="../data/chroma_{}".format(db_name))
-collection = client.create_collection(db_name)
-collection.add(
-    ids=[str(i) for i in range(0, len(textual_knowledge))], 
-    documents=textual_knowledge,
-    metadatas=[{"type": metadata_name} for _ in range(0, len(textual_knowledge))]
-)
 
-tot_time = round((time.time() - start_time)/(60*60), 2)
-print("Database is populated in {} hr".format(tot_time))
+def load_data():
+    with open(DATA_PATH, "rb") as f:
+        data = pickle.load(f)
+    metadata_list = list(map(lambda x:{"source": x + " from SPOKE knowledge graph"}, data))
+    return data, metadata_list
+
+def create_vectordb():
+    start_time = time.time()
+    data, metadata_list = load_data()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    docs = text_splitter.create_documents(data, metadatas=metadata_list)
+    batches = [docs[i:i + BATCH_SIZE] for i in range(0, len(docs), BATCH_SIZE)]
+    vectorstore = Chroma(embedding_function=SentenceTransformerEmbeddings(model_name=SENTENCE_EMBEDDING_MODEL), 
+                         persist_directory=VECTOR_DB_NAME)
+    for batch in batches:
+        vectorstore.add_documents(documents=batch)
+    end_time = round((time.time() - start_time)/(60*60), 2)
+    print("VectorDB is created in {} hrs".format(end_time))
+
+
+if __name__ == "__main__":
+    create_vectordb()
+    
