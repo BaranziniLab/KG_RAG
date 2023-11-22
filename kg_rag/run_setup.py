0075d7020e20efe511149066bf3a68729e8d5627
Karthik Soman <karthi.soman@gmail.com>
2023-11-21 16:26:58 -0800

    changed the folder name

--- kg_rag/run_setup.py
+++ kg_rag/run_setup.py
@@ -10,5 +10,5 @@ if os.path.exists(config_data["VECTOR_DB_PATH"]):
     print("vectorDB already exists!")
 else:
     print("Creating vectorDB ...")
-    from kg_rag.codes.py_scripts.vectorDB.create_vectordb import create_vectordb
+    from kg_rag.vectorDB.create_vectordb import create_vectordb
     create_vectordb()
