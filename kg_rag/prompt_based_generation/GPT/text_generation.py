0a6fdc0c4c8c3bddc98d99a9e8e3973969ac9aad
Karthik Soman <karthi.soman@gmail.com>
2023-11-22 01:34:36 -0800

    adding a streaming fn to gpt text generation prmpt based

--- kg_rag/prompt_based_generation/GPT/text_generation.py
+++ kg_rag/prompt_based_generation/GPT/text_generation.py
@@ -15,9 +15,18 @@ def main():
     print("Here is the prompt-based answer:")
     print("")
     output = get_GPT_response(question, SYSTEM_PROMPT, CHAT_MODEL_ID, CHAT_DEPLOYMENT_ID, temperature=TEMPERATURE)
-    print(output)
+    stream_out(output)
     
-
+def stream_out(output):
+    CHUNK_SIZE = 10
+    SLEEP_TIME = 0.1
+    for i in range(0, len(output), CHUNK_SIZE):
+        print(output[i:i+CHUNK_SIZE], end='')
+        sys.stdout.flush()
+        time.sleep(SLEEP_TIME)
+    
+    
+    
 if __name__ == "__main__":
     main()
 
