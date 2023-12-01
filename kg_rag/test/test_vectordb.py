1b752f8b4047783aca6283d1f44aa0ea7abad3d1
Karthik Soman <karthi.soman@gmail.com>
2023-11-30 21:13:54 -0800

    added extra print functions to vectorDB test script

--- kg_rag/test/test_vectordb.py
+++ kg_rag/test/test_vectordb.py
@@ -5,22 +5,31 @@ VECTOR_DB_PATH = config_data["VECTOR_DB_PATH"]
 SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL = config_data["SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL"]
 
 print("Testing vectorDB loading ...")
+print("")
 try:
     vectorstore = load_chroma(VECTOR_DB_PATH, SENTENCE_EMBEDDING_MODEL_FOR_NODE_RETRIEVAL)
     print("vectorDB is loaded succesfully!")
+    print("")
     print("Testing entity extraction ...")
+    print("")
     entity = "psoriasis"
     print("Inputting '{}' as the entity".format(entity))    
+    print("")
     node_search_result = vectorstore.similarity_search_with_score(entity, k=1)
     extracted_entity = node_search_result[0][0].page_content
-    print("Extracted entity is ", extracted_entity)
+    print("Extracted entity is '{}'", extracted_entity)
+    print("")
     if extracted_entity == "psoriasis":                
         print("Entity extraction is successful!")
+        print("")
+        print("vectorDB is correctly populated and is good to go!")
     else:
         print("Entity extraction is not successful. Make sure vectorDB is populated correctly. Refer 'How to run KG-RAG' Step 5")
+        print("")
         sys.exit(1)
 except:
     print("vectorDB is not loaded. Check the path given in 'VECTOR_DB_PATH' of config.yaml")
+    print("")
     sys.exit(1)
     
     
