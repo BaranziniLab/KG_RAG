46c27c99b109e00dd39ef7aabb36e7983560852c
Karthik Soman <karthi.soman@gmail.com>
2023-12-05 23:16:00 -0800

    added method arg to prompt based llama

--- kg_rag/prompt_based_generation/Llama/text_generation.py
+++ kg_rag/prompt_based_generation/Llama/text_generation.py
@@ -1,5 +1,12 @@
 from langchain import PromptTemplate, LLMChain
 from kg_rag.utility import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', type=str, default='method-1', help='Method to choose for Llama model')
+args = parser.parse_args()
+
+METHOD = args.m
 
 
 SYSTEM_PROMPT = system_prompts["PROMPT_BASED_TEXT_GENERATION"]
@@ -13,7 +20,7 @@ INSTRUCTION = "Question: {question}"
 
 
 def main():
-    llm = llama_model(MODEL_NAME, BRANCH_NAME, CACHE_DIR, stream=True)
+    llm = llama_model(MODEL_NAME, BRANCH_NAME, CACHE_DIR, stream=True, method=METHOD)
     template = get_prompt(INSTRUCTION, SYSTEM_PROMPT)
     prompt = PromptTemplate(template=template, input_variables=["question"])    
     llm_chain = LLMChain(prompt=prompt, llm=llm)
