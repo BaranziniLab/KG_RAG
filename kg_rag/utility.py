57ecf27e974ad9225c0332f6e41289defd440727
Karthik Soman <karthi.soman@gmail.com>
2024-02-28 16:56:03 -0800

    addingperiod and space to extracted context

--- kg_rag/utility.py
+++ kg_rag/utility.py
@@ -365,10 +365,11 @@ def interactive(question, vectorstore, node_context_df, embedding_function_for_c
         if len(high_similarity_indices) > max_number_of_high_similarity_context_per_node:
             high_similarity_indices = high_similarity_indices[:max_number_of_high_similarity_context_per_node]
         high_similarity_context = [node_context_list[index] for index in high_similarity_indices]
-        high_similarity_context = list(map(lambda x:x+'. ', high_similarity_context))
+        high_similarity_context = list(map(lambda x:x+'. ', high_similarity_context))        
         if edge_evidence:
             context_table = context_table[context_table.context.isin(high_similarity_context)]
             print(high_similarity_context[0])
+            print(context_table.context.values[0])
             print(context_table)
             context_table.loc[:, "context"] =  context_table.source + " " + context_table.predicate.str.lower() + " " + context_table.target + " and Provenance of this association is " + context_table.provenance + " and attributes associated with this association is in the following JSON format:\n " + context_table.evidence.astype('str') + "\n\n"                
             node_context_extracted = context_table.context.str.cat(sep=' ')
